43a46b0db1f467cdac8708fef8a72db3e7072d0a
Mabufudyne <lukas.knapek@kolabnow.com>
2020-03-18 09:52:47 +0000

    Integrating tasks 3 and 4 with tasklist, linking the list to quiz

--- app/src/main/java/com/example/knowyourcampus/Task3Main.java
+++ app/src/main/java/com/example/knowyourcampus/Task3Main.java
@@ -1,7 +1,9 @@
 package com.example.knowyourcampus;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -24,14 +26,35 @@ public class Task3Main extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-
-                Intent intent = new Intent(Task3Main.this, Task3Camera.class);
-                startActivity(intent);
-
+            Intent intent = new Intent(Task3Main.this, Task3Camera.class);
+            startActivity(intent);
             }
         });
 
     }
 
+    public void onGiveUpClick(final View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Task3Main.this);
+        builder.setMessage("Do you really want give up on this task? You will see the answer, but won't gain any points!");
+        builder.setTitle("Give up?");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Give up", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(v.getContext(), TaskListActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXTRA_REPRODUCTION_TASK_COMPLETED", true);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
 
 }
--- app/src/main/java/com/example/knowyourcampus/Task4Completed.java
+++ app/src/main/java/com/example/knowyourcampus/Task4Completed.java
@@ -2,9 +2,11 @@ package com.example.knowyourcampus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class Task4Completed extends AppCompatActivity {
@@ -20,5 +22,12 @@ public class Task4Completed extends AppCompatActivity {
         Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
         ImageView image = (ImageView) findViewById(R.id.selfie);
         image.setImageBitmap(bmp);
+    }
+
+    public void onContinueClick(View v) {
+        Intent intent = new Intent(this, TaskListActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("EXTRA_SELFIE_TASK_COMPLETED", true);
+        startActivity(intent);
     }
 }
